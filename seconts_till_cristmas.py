d3579f0a431f9ac6ca10796b608727fdbc622f9a
DevEatSleepRepeat <108988616+DevEatSleepRepeat@users.noreply.github.com>
2023-12-03 20:01:07 -0500

    Update seconts_till_cristmas.py

--- seconts_till_cristmas.py
+++ seconts_till_cristmas.py
@@ -10,4 +10,4 @@ sect = int(today.strftime('%S'))
 christmas = datetime(year, 12, 25)
 day_till_christmas = dd(today, christmas)
 seconds_till_christmas = (((day_till_christmas*24-hour)*60-miut)*60-sect)
-print(f'You Have {seconds_till_christmas} seconds till Christmas!')ss
+print(f'You Have {seconds_till_christmas} seconds till Christmas!')
