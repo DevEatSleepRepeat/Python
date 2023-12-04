7998f9f3ef2529dbfb7c476061bdb29cd0ca5f0b
DevEatSleepRepeat <108988616+DevEatSleepRepeat@users.noreply.github.com>
2023-12-03 19:56:24 -0500

    Add files via upload

---
+++ seconts_till_cristmas.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+def dd(datetime1, datetime2):
+    delta = datetime2 - datetime1
+    return delta.days
+today = datetime.now()
+year = int(today.strftime('%Y'))
+hour = int(today.strftime('%H'))
+miut = int(today.strftime('%M'))
+sect = int(today.strftime('%S'))
+christmas = datetime(year, 12, 25)
+day_till_christmas = dd(today, christmas)
+seconds_till_christmas = (((day_till_christmas*24-hour)*60-miut)*60-sect)
+print(f'You Have {seconds_till_christmas} seconds till Christmas!')ss
